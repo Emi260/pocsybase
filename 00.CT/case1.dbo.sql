139af425f608b1a6ec0397e67db193f8d610ef13
Emi260 <65983384+Emi260@users.noreply.github.com>
2022-06-09 17:03:46 -0300

    se cambia el CT

--- 00.CT/case1.dbo.sql
+++ 00.CT/case1.dbo.sql
@@ -1,8 +1,13 @@
 --liquibase formatted sql
 
 --changeset case:1
-create table users(
-  id int primary key,
-  name varchar(30),
-  keys  varchar(10)
-)
+CREATE TABLE SalesOrders2 (
+FinancialCode CHAR(2),
+CustomerID int IN Dsp1,
+History CLOB IN Dsp2,
+OrderDate TIMESTAMP,
+ID BIGINT
+PRIMARY KEY(ID) IN Dsp4 
+) IN Dsp3
+
+--rollback DROP TABLE SalesOrders2;
