0e24c71c8bdbfbd1ee5f48b023fed1055a2c1f86
Emi260 <65983384+Emi260@users.noreply.github.com>
2022-06-09 17:05:18 -0300

    cambio en CT

--- 00.CT/case1.dbo.sql
+++ 00.CT/case1.dbo.sql
@@ -3,11 +3,8 @@
 --changeset case:1
 CREATE TABLE SalesOrders2 (
 FinancialCode CHAR(2),
-CustomerID int IN Dsp1,
-History CLOB IN Dsp2,
 OrderDate TIMESTAMP,
 ID BIGINT
-PRIMARY KEY(ID) IN Dsp4 
-) IN Dsp3
+)
 
 --rollback DROP TABLE SalesOrders2;
