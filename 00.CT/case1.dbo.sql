5749128dca03df1c4eec67d6afd841244cdeff94
Emi260 <65983384+Emi260@users.noreply.github.com>
2022-06-09 17:08:26 -0300

    1

--- 00.CT/case1.dbo.sql
+++ 00.CT/case1.dbo.sql
@@ -2,7 +2,7 @@
 
 --changeset case:1
 CREATE TABLE library_books (
-isbn CHAR(20)      PRIMARY KEY IQ UNIQUE (150000),
+isbn CHAR(20),
 copyright_date     DATE,
 title              CHAR(100),
 author             CHAR(50)
