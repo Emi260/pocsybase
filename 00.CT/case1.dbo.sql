37db439d46d0841442835f581a3e056123bce297
Emi260 <65983384+Emi260@users.noreply.github.com>
2022-06-13 11:02:31 -0300

    se modifica el Case1

--- 00.CT/case1.dbo.sql
+++ 00.CT/case1.dbo.sql
@@ -10,7 +10,6 @@ author             CHAR(50)
 
 --rollback DROP TABLE library_books;
 
-
 --changeset case:2
 CREATE TABLE cuenta (
 nombre CHAR(20),
@@ -19,3 +18,12 @@ author             CHAR(50)
 )
 
 --rollback DROP TABLE cuenta;
+
+--changeset case:3
+CREATE TABLE test (
+nombre CHAR(20),
+title              CHAR(100),
+author             CHAR(50)
+)
+
+--rollback DROP TABLE test;
